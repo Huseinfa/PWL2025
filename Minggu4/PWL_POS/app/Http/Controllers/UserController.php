aee63b0addc1746868f5d2f162ff66d864563f96
= <=>
2025-03-11 21:22:39 +0700

    add: js4 prak 2.2

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,21 +10,6 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data user dengan Eloquent Model
-        // $data = [
-        //     // 'username' => 'customer-1',
-        //     // 'nama' => 'Pelanggan',
-        //     // 'password' => Hash::make('12345'),
-        //     // 'level_id' => 5
-
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // // UserModel::insert($data); // tambahkan data ke tabel m_user
-        // UserModel::where('username','customer-1')->update($data); // update data user
-
-        // //coba akses model UserModel
-        // $user = UserModel::all(); // ambil semua data
-        // return view('user', ['data' => $user]);
 
         #js4 prak 1.1
         // $data = [
@@ -51,28 +36,38 @@ class UserController extends Controller
         // $user = UserModel::all();
         // return view('user', ['data' => $user]);
 
-        #js4 prak 2.1
+        #js4 prak 2.1.1
         // $user = UserModel::find(1);
         // return view('user', ['data' => $user]);
 
-        #js4 prak 2.2
+        #js4 prak 2.1.2
         // $user = UserModel::where('level_id', 1)->first();
         // return view('user', ['data' => $user]);
 
-        #js4 prak 2.3
+        #js4 prak 2.1.3
         // $user = UserModel::firstWhere('level_id',1);
         // return view('user', ['data' => $user]);
         
-        #js4 prak 2.4
+        #js4 prak 2.1.4
         // $user = UserModel::findOr(1, ['username', 'nama'], function(){
         //     abort(404);
         // });
         // return view('user', ['data' => $user]);
         
-        #js4 prak 2.5
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        #js4 prak 2.1.5
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+        
+        #js4 prak 2.2.1
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+        
+        #js4 prak 2.2.2
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
+
+        
     }
 }
