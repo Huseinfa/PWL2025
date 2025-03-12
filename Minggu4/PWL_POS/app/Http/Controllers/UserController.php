9cce75d58b254631cae3b31d01b8db0d183da396
= <=>
2025-03-12 09:41:33 +0700

    add: js4 prak 2.4

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -68,11 +68,61 @@ class UserController extends Controller
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
         // return view('user', ['data' => $user]);
 
-        #js4 prak 2.3.1
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
-        return view('user', ['data' => $user]);
+        #js4 prak 2.3
+        // $user = UserModel::where('level_id', 2)->count();
+        // // dd($user);
+        // return view('user', ['data' => $user]);
 
+        #js4 prak 2.4.1
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        // return view('user', ['data' => $user]);
+
+        #js4 prak 2.4.2
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        // );
+        // return view('user', ['data' => $user]);
         
+        #js4 prak 2.4.3
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        // return view('user', ['data' => $user]);
+
+        #js4 prak 2.4.4
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        // );
+        // return view('user', ['data' => $user]);
+
+        #js4 prak 2.5
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ],
+        );
+        $user->save();
+        return view('user', ['data' => $user]);
     }
 }
