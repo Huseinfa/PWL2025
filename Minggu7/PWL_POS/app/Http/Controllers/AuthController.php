b5166c90ebbfc2c4189631d06aa2b02fdc60cdd3
= <=>
2025-04-13 19:57:35 +0700

    add: tugas 4 + laporan

--- Minggu7/PWL_POS/app/Http/Controllers/AuthController.php
+++ Minggu7/PWL_POS/app/Http/Controllers/AuthController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LevelModel;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login()
     {
-        if (Auth::check()) { // jika sudah login, maka redirect ke halaman home 
+        if(Auth::check()){ // jika sudah login, maka redirect ke halaman home 
             return redirect('/');
         }
         return view('auth.login');
@@ -17,18 +20,17 @@ class AuthController extends Controller
 
     public function postlogin(Request $request)
     {
-        if ($request->ajax() || $request->wantsJson()) {
+        if($request->ajax() || $request->wantsJson()){
             $credentials = $request->only('username', 'password');
-
-            if (Auth::attempt($credentials)) {
+            if (Auth::attempt($credentials)) { 
                 return response()->json([
                     'status' => true,
-                    'message' => 'Login Berhasil',
+                    'message' => 'Login Berhasil', 
                     'redirect' => url('/')
                 ]);
             }
-            return response()->json([
-                'status' => false,
+            return response()->json([ 
+                'status' => false, 
                 'message' => 'Login Gagal'
             ]);
         }
@@ -41,7 +43,45 @@ class AuthController extends Controller
         Auth::logout();
 
         $request->session()->invalidate();
-        $request->session()->regenerateToken();
+        $request->session()->regenerateToken(); 
         return redirect('login');
+    }
+
+    public function register()
+    {
+        $levels = LevelModel::select('level_id', 'level_nama')->get();
+        
+        return view('auth.register')->with('levels', $levels);
+    }
+
+    public function postRegister(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|unique:m_user,username',
+            'nama' => 'required|string|max:255',
+            'password' => 'required|string|min:5|confirmed',
+            'level_id' => 'required|exists:m_level,level_id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Failed.',
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Registration Success.',
+            'redirect' => url('login')
+        ]);
     }
 }
