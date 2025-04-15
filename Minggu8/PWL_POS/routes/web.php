88de5fa631a225e4e6beba55c445d0d0d616fe42
= <=>
2025-04-15 17:13:17 +0700

    add: js8 tugas 2

--- Minggu8/PWL_POS/routes/web.php
+++ Minggu8/PWL_POS/routes/web.php
@@ -47,71 +47,53 @@ Route::middleware('auth')->group(function () {
         Route::group(['prefix' => 'user'], function () {
             Route::get('/',[UserController::class, 'index']);
             Route::post('/list',[UserController::class, 'list']);
-            Route::get('/create',[UserController::class, 'create']);
-            Route::post('/',[UserController::class, 'store']);
             Route::get('/create_ajax',[UserController::class, 'create_ajax']);
             Route::post('/ajax',[UserController::class, 'store_ajax']);
-            Route::get('/{id}',[UserController::class, 'show']);
-            Route::get('/{id}/edit',[UserController::class, 'edit']);
-            Route::put('/{id}',[UserController::class, 'update']);
             Route::get('/{id}/edit_ajax',[UserController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax',[UserController::class, 'update_ajax']);
-            Route::delete('/{id}',[UserController::class, 'destroy']);
             Route::get('/{id}/delete_ajax',[UserController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax',[UserController::class, 'delete_ajax']);
             Route::get('/import', [UserController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [UserController::class, 'import_ajax']); // ajax import excel
+            Route::get('/export_excel', [UserController::class, 'export_excel']); // export excel
         });
         
         Route::group(['prefix' => 'level'], function () {
             Route::get('/',[LevelController::class, 'index']);
             Route::post('/list',[LevelController::class, 'list']);
-            Route::get('/create',[LevelController::class, 'create']);
-            Route::post('/',[LevelController::class, 'store']);
             Route::get('/create_ajax',[LevelController::class, 'create_ajax']);
             Route::post('/ajax',[LevelController::class, 'store_ajax']);
-            Route::get('/{id}',[LevelController::class, 'show']);
-            Route::get('/{id}/edit',[LevelController::class, 'edit']);
-            Route::put('/{id}',[LevelController::class, 'update']);
             Route::get('/{id}/edit_ajax',[LevelController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax',[LevelController::class, 'update_ajax']);
-            Route::delete('/{id}',[LevelController::class, 'destroy']);
             Route::get('/{id}/delete_ajax',[LevelController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax',[LevelController::class, 'delete_ajax']);
             Route::get('/import', [LevelController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [LevelController::class, 'import_ajax']); // ajax import excel
+            Route::get('/export_excel', [LevelController::class, 'export_excel']); // export excel
         });
         
         Route::group(['prefix' => 'supplier'], function () {
-            Route::get('/create',[SupplierController::class, 'create']);
-            Route::post('/',[SupplierController::class, 'store']);
             Route::get('/create_ajax',[SupplierController::class, 'create_ajax']);
             Route::post('/ajax',[SupplierController::class, 'store_ajax']);
-            Route::get('/{id}/edit',[SupplierController::class, 'edit']);
-            Route::put('/{id}',[SupplierController::class, 'update']);
             Route::get('/{id}/edit_ajax',[SupplierController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax',[SupplierController::class, 'update_ajax']);
-            Route::delete('/{id}',[SupplierController::class, 'destroy']);
             Route::get('/{id}/delete_ajax',[SupplierController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax',[SupplierController::class, 'delete_ajax']);
             Route::get('/import', [SupplierController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [SupplierController::class, 'import_ajax']); // ajax import excel
+            Route::get('/export_excel', [SupplierController::class, 'export_excel']); // export excel
         });
         
         Route::group(['prefix' => 'kategori'], function () {
-            Route::get('/create',[KategoriController::class, 'create']);
-            Route::post('/',[KategoriController::class, 'store']);
             Route::get('/create_ajax',[KategoriController::class, 'create_ajax']);
             Route::post('/ajax',[KategoriController::class, 'store_ajax']);
-            Route::get('/{id}/edit',[KategoriController::class, 'edit']);
-            Route::put('/{id}',[KategoriController::class, 'update']);
             Route::get('/{id}/edit_ajax',[KategoriController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax',[KategoriController::class, 'update_ajax']);
-            Route::delete('/{id}',[KategoriController::class, 'destroy']);
             Route::get('/{id}/delete_ajax',[KategoriController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax',[KategoriController::class, 'delete_ajax']);
             Route::get('/import', [KategoriController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [KategoriController::class, 'import_ajax']); // ajax import excel
+            Route::get('/export_excel', [KategoriController::class, 'export_excel']); // export excel
         });
     });
     
