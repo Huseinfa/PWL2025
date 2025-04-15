ea153ba1d105adafadfb56fc86f486fff18e1104
= <=>
2025-04-15 20:55:41 +0700

    feat: import export

--- POS/app/Models/User.php
+++ POS/app/Models/User.php
@@ -2,44 +2,48 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class UserModel extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    protected $hidden = ['password'];
+
+    protected $casts = ['password' => 'hashed'];
+
+    public function level(): BelongsTo 
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+     /**
+     * Mendapatkan nama role
+     */
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
+     * Cek apakah user memiliki role tertentu
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
+     * Mendapatkan kode role
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
 }
